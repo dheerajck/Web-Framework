6a173edafe84470fe5118ec25719317afaa175f3
dheeraj.c <dheeraj.c@qburst.com>
2022-01-24 10:44:15 +0530

    Added requirements needed for the webapp to run properly

--- webapp/views/authentication_views.py
+++ webapp/views/authentication_views.py
@@ -19,6 +19,7 @@ def authenticating_view(environ, **kwargs):
         return redirect_to_login_module()
 
     form_field_storage = form_with_file_parsing(environ)
+
     username = form_field_storage.getvalue('username')
     password = form_field_storage.getvalue('password')
     print(username, password)
@@ -41,6 +42,8 @@ def authenticating_view(environ, **kwargs):
         # http_host_is_not_needed_in_current_case
         # http_host = environ.get('HTTP_HOST')
         # redirect_url_path = '/dashboard/'
+        # button = form_field_storage.getvalue('password')
+        # if form_field_storage.getvalue('input_pressed') == 'regiser':
 
         # Launched external handler for 'localhost:8000//dashboard/'.
         # url_to_redirect = f'{http_host}/{redirect_url_path}'
@@ -81,7 +84,7 @@ def logout_view(environ, **kwargs):
     # print(cookie_string is None)
 
     # we need to make sure HTTP_COOKIE is not None, so cookie_string is not None should return True to proceed
-    assert cookie_string is not None, "No cookies, then how stayed in the website till now"
+    # assert cookie_string is not None, "No cookies, then how stayed in the website till now"
     # get our apps session_id from cookies
     cookie_dict = get_cookie_dict(cookie_string)
     # print()
@@ -90,8 +93,10 @@ def logout_view(environ, **kwargs):
 
     # session_key_value = cookie_dict.get(SESSION_KEY_NAME)
     # if condition false raise error
-    assert SESSION_KEY_NAME in cookie_dict, "Again asking, No cookies, then how stayed in the website till now"
-    session_key_value = cookie_dict[SESSION_KEY_NAME]
-    delete_session_id(session_key_value)
+    # assert SESSION_KEY_NAME in cookie_dict, "Again asking, No cookies, then how stayed in the website till now"
+    # removed assertion and added this check to avoid assertion error and key error if user directly go to logout url for somereason
+    if SESSION_KEY_NAME in cookie_dict:
+        session_key_value = cookie_dict[SESSION_KEY_NAME]
+        delete_session_id(session_key_value)
 
     return redirect_to_login_module()
